cc9784b9b23f6c46c9c9fca4cb47ae84800a1da2
Tyler Hatfield <hatfield.tyler@gmail.com>
2020-07-01 15:03:22 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const exphbs  = require('express-handlebars');
+const path = require('path');
+
+
+const PORT = process.env.PORT || 7000;
+
+//set handlebars Middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+const otherstuff = "hello there, this is other stuff";
+
+//set handlebar routes
+app.get('/', function (req, res) {
+    res.render('home', {
+    	stuff: "This is stuff...",
+    	stuff1: otherstuff
+    });
+
+});
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+
+app.listen(PORT, () => console.log('Server listening on port ' + PORT));
