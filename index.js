d9cd4c66fcfca77cf7e4c837f2969da98595ffef
Tyler Hatfield <hatfield.tyler@gmail.com>
2020-07-02 11:53:31 -0400

    created procfile for heroku

--- index.js
+++ index.js
@@ -4,9 +4,30 @@ const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
 const path = require('path');
-
+const request = require('request');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 7000;
+
+
+// user body parser Middleware
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+
+
+//API KEY pk_b5523af07d924bd091cfcae3a1bda8a7 
+function call_api(finishedAPI, ticker){
+request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_b5523af07d924bd091cfcae3a1bda8a7', { json: true }, (err, res, body) => {
+	if(err) {return console.log(err);}
+		if (res.statusCode === 200){
+		//console.log(body);
+		finishedAPI(body);
+		};
+	});	
+};
+
 
 //set handlebars Middleware
 app.engine('handlebars', exphbs());
@@ -14,13 +35,28 @@ app.set('view engine', 'handlebars');
 
 const otherstuff = "hello there, this is other stuff";
 
-//set handlebar routes
+//set handlebar index routes
 app.get('/', function (req, res) {
-    res.render('home', {
-    	stuff: "This is stuff...",
-    	stuff1: otherstuff
-    });
+	call_api(function(doneAPI){
+		res.render('home', {
+    		stock: doneAPI
+   		 });
+	}, 'fb');
+});
+//call_api(function, req.body.stock_ticker )
+//set handlebar index post routes
+app.post('/', function (req, res) {
+	call_api(function(doneAPI){
+		//posted_report = req.body.stock_ticker;
+			res.render('home', {
+    		stock: doneAPI,	
+   		 });
+	}, req.body.stock_ticker);
+});
 
+//set about page route
+app.get('/about.html', function (req, res) {
+    res.render('about');
 });
 
 //set static folder
